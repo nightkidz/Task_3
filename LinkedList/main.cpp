7342746274cc3ecb6971793467ecb6c07d6ff6fb
Alfian Pamungkas Sakawiguna <alfian@metroworld.co.id>
2015-09-16 23:29:54 +0700

    Update main.cpp

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -1,9 +1,35 @@
+//Anggota Kelompok:
+//Fachri Ul Albab(1301140151)
+//Alfian Pamuungkas S(1301144411)
+//Ezekiel Manurung(
+//IF-38-01
 #include <iostream>
+#include <conio.h>
+#include <stdio.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    void menu;
+    switch(menu);
+    {
+    case 1:
+        void ins_first;
+    case 2:
+        void ins_after;
+    case 3:
+        void ins_last;
+    case 4:
+        void del_first;
+    case 5:
+        void del_after;
+    case 6:
+        void del_last;
+    default:
+        cout<<"Wrong Input"<<endl;
+        break;
+    }
+
     return 0;
 }
