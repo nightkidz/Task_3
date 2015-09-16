163a274f26224e66f45e03f948dc919753eba309
Alfian Pamungkas Sakawiguna <alfian@metroworld.co.id>
2015-09-16 23:29:00 +0700

    Update list.h

--- LinkedList/list.h
+++ LinkedList/list.h
@@ -1,6 +1,20 @@
+//Anggota Kelompok:
+//Fachri Ul Albab(1301140151)
+//Alfian Pamuungkas S(1301144411)
+//Ezekiel Manurung(
+//IF-38-01
 #ifndef LIST_H_INCLUDED
 #define LIST_H_INCLUDED
 
-
+struct user
+struct elementlist
+void menu()
+void ins_first(elementlist *p)
+void ins_after(elementlist *p,elementlist *q)
+void ins_last(elementlist *p,elementlist *q)
+void del_first(elementlist *p)
+void del_after(elementlist *p,elementlist *q)
+void del_last(elemenlist *p,elementlist *q)
+void view_list(elemenlist *p)
 
 #endif // LIST_H_INCLUDED
